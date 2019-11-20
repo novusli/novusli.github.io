dda3b9c4ea7d832513840db945393e2d3bae9138
novusli <novuslzq@gmail.com>
2019-11-20 07:24:12 -0800

    Upload portfolio files

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const navItems = document.querySelectorAll('.nav-link');
+
+// Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+menu.addEventListener('click', closeMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // Set Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // Set Menu State
+        showMenu = false;
+    }
+}
+
+function closeMenu() {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    // Set Menu State
+    showMenu = false;
+}
